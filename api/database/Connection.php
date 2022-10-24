d23405117efc7ab93012cbf759e3524405177e60
Felipe Bueno <f.dacunhabueno@gmail.com>
2022-10-23 22:14:15 -0300

    Versão 1.1.9

--- api/database/Connection.php
+++ api/database/Connection.php
@@ -5,7 +5,6 @@
     define('PASSWORD', '');
     define('DATABASE', 'books');
     //debug//
-    // define('PORT', '8012'); //Porta do banco de dados de casa
     define('PORT', '3307'); //Porta do banco de dados de casa
     $mysqli = new mysqli(SERVER, USERNAME, PASSWORD, DATABASE, PORT) or die ('Not able to connect!');
 ?>
--- view/home.php
+++ view/home.php
@@ -1,29 +1,38 @@
 <?php
-?>
-<br>
-<div class="card container" style="width: 32rem;">
+
+    include_once 'api/database/Connection.php';
+    $SQL = 'Select * from posts';
+    $record = $mysqli->query($SQL) or die ('Falha ao se conectar ao servidor!');
+    while ($row = $record->fetch_assoc()) {
+        echo '
+    <br>
+    <div class="card container" style="width: 32rem;">
     <div class="card-header d-flex justify-content-end">
-    <p class="card-title user-title"> <em>Luis Roberto</em> publicou...</p>
+    <p class="card-title user-title">' . $_SESSION['username'] . 'publicou...</p
         <div class="user-img-div">
-            <img src="image/Jotaro Kujo.png" class="mx-auto  user-img-fit" alt="...">
+            <img src="image/Jotaro Kujo.png" class="mx-auto  user-img-fit" alt="..."> 
         </div>
     </div>
     <div class="img-div">
-        <img src="image/gato.png" class="mx-auto img-fit img-thumbnail " alt="...">
+        <img src="res/app-img/Blogs/'. $row['image'] .'" class="mx-auto img-fit img-thumbnail " alt="...">
     </div>
     <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <h5 class="card-title">'.$row['Title'].'</h5>
+        <p class="card-text">'.$row['content'].'.</p>
     </div>
     <ul class="list-group list-group-flush">
-        <li class="list-group-item">An item</li>
-        <li class="list-group-item"><p class="data-hora"><em>Publicado em 20/10/2022</em></p></li>
-</div>
+        <li class="list-group-item">'. $row['Interesses'].'</li>
+        <li class="list-group-item"><p class="data-hora"><em>'.$row['CreatedAt'].'</em></p></li>
+    </div>
+        ';
+    }
+?>
+
 <!-- O trecho a seguir só consta como exemplo, a idéia é que esta funcionalidade seja mecanizada e automatizada. -->
 <br>
 <div class="card container" style="width: 32rem;">
     <div class="card-header d-flex justify-content-end">
-    <p class="card-title user-title"> <em>Luis Roberto</em> publicou...</p>
+    <p class="card-title user-title"> <em><?php echo $_SESSION['username'] ?></em> publicou...</p>
         <div class="user-img-div">
             <img src="image/Jotaro Kujo.png" class="mx-auto  user-img-fit" alt="...">
         </div>
